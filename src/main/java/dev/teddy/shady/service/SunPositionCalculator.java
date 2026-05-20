57a4946fa18e428edbc7b60a4f1d15e3ad789a4f
Teddy's playground <164116423+Teddy50060@users.noreply.github.com>
2026-05-20 18:22:17 +0700

    sun calculation fixed

--- src/main/java/dev/teddy/shady/service/SunPositionCalculator.java
+++ src/main/java/dev/teddy/shady/service/SunPositionCalculator.java
@@ -14,7 +14,7 @@ public class SunPositionCalculator {
         int minute = dateTime.getMinute();
         double timeZoneOffset = currentLongitude / 15;
         double gamma = 2 * Math.PI * (day - 1 + (hour - 12) * 1.0 / 24);
-        double solarDeclination = 0.006918 - 0.399912 * Math.cos(gamma) - 0.006758 * Math.cos(2 * gamma) + 0.000907 * Math.sin(2 * gamma);
+        double solarDeclination = 0.006918 - 0.399912 * Math.cos(gamma) + 0.070257*Math.sin(gamma) - 0.006758 * Math.cos(2 * gamma) + 0.000907 * Math.sin(2 * gamma);
         double equationOfTime = 229.18 * (0.000075 + 0.001868 * Math.cos(gamma) - 0.032077 * Math.sin(gamma) - 0.014615 * Math.cos(2 * gamma) - 0.040849 * Math.sin(2 * gamma));
         double trueSolarTime = (60 * hour + minute) + 4 * currentLongitude - 60 * timeZoneOffset + equationOfTime;
         double solarHourAngle = (trueSolarTime - 720) * 0.25;
